527575f045fe58f4668f970fd58acd7a019fcc53
Rohit Moni <rohitmdxb@gmail.com>
2016-01-30 12:14:03 -0800

    Pentagram Prefab

---
+++ Assets/Scripts/PentagramController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PentagramController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Pentagram collision detected");
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.SendMessage("OverPentagram", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Debug.Log("Pentagram collision left");
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.SendMessage("OverPentagram", false);
+        }
+    }
+}
