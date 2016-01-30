8039d7573c1da914de574abc8da8c72a8d1765bd
CailanStm <cailanstm@shaw.ca>
2016-01-30 14:29:07 -0800

    Basic Main Menu

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public void StartGameButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void QuitGameButton()
+    {
+        Application.Quit();
+    }
+}
