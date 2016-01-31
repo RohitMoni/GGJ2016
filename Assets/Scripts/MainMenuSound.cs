d495bb1f1e1169d36238ddbc681846186ef32c2d
Rohit Moni <rohitmdxb@gmail.com>
2016-01-31 12:52:09 -0800

    main menu sound

---
+++ Assets/Scripts/MainMenuSound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuSound : MonoBehaviour
+{
+    public AudioClip menusound;
+
+	// Use this for initialization
+	void Start () {
+	    if (menusound != null)
+	    {
+            AudioSource.PlayClipAtPoint(menusound, Camera.main.transform.position);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
