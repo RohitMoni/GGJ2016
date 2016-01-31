f003e94e9f660a9729942a413e842def70a438ca
Ian McCall <ianmccall93@gmail.com>
2016-01-31 11:48:47 -0800

    Added filmGrainOverlay script.

---
+++ Assets/Scripts/FilmGrainOverlay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FilmGrainOverlay : MonoBehaviour {
+
+    SpriteRenderer spriteRender;
+    public float animationFrequency = 12f;
+    public List<Sprite> filmGrainSprites;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    private float timeSinceTransition = 0;
+	void Update () {
+        timeSinceTransition += Time.deltaTime;
+        if(timeSinceTransition > 1 / animationFrequency)
+        {
+            timeSinceTransition = 0;
+
+            var frame = (int)Random.Range(0, filmGrainSprites.Count - 1);
+            int randomFrame = (int)(Random.value * (filmGrainSprites.Count - 1));
+
+            spriteRender.sprite = filmGrainSprites[frame];
+        }
+	}
+}
